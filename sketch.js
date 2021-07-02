7207f5d0668d8e791c55094c2489d88461ec421c
RohitJain2007 <78537957+RohitJain2007@users.noreply.github.com>
2021-07-02 20:04:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function preload(){
+  backImg=loadImage("images/bg img.png")
+  moonImg=loadAnimation("images/moon1.png","images/moon2.png","images/moon3.png","images/moon4.png")
+  dragImg=loadAnimation("images/drag 1.png","images/drag 2.png","images/drag 3.png","images/drag 4.png","images/drag 5.png")
+  obstacleImg=loadImage("images/bamboo.png")
+}
+function setup() 
+{
+  createCanvas(650,500);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
